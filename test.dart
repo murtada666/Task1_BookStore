ac7068fb06b9060ed0e99eff33d0ff330dd392a2
murtada666 <91660848+murtada666@users.noreply.github.com>
2022-08-13 22:33:46 +0300

    Create test.dart

---
+++ test.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+
+import 'classes.dart';
+
+void main(List<String> args) {
+  List<Book> myBooks = <Book>[];
+
+  int? userInput;
+
+  switch (userInput) {
+    case 1:
+      {
+        if (myBooks.isNotEmpty) {
+          myBooks.forEach((element) {
+            print(element);
+          });
+        } else {
+          print("there are no books");
+        }
+      }
+      break;
+
+    case 2:
+      {
+        myBooks.forEach((element) {
+          if (element == userInput) {
+            print(element);
+          }
+        });
+      }
+      break;
+
+    case 3:
+      {
+        print("Enter the book name: ");
+        String? name = stdin.readLineSync();
+
+        print("Enter the book author: ");
+        String? auther = stdin.readLineSync();
+
+        print("Enter the book rate: ");
+        double? rate = double.parse(stdin.readLineSync()!);
+        
+      }
+      break;
+
+    case 4:
+      {
+        myBooks.forEach((element) {
+          if (element.getName == userInput) {
+            String? name = stdin.readLineSync();
+            String? auther = stdin.readLineSync();
+            double? rate = double.parse(stdin.readLineSync()!);
+
+            element.setName(name!);
+            element.setAuther(auther!);
+            element.setRate(rate);
+          }
+        });
+      }
+      break;
+
+    case 5:
+      {
+        myBooks.forEach((element) {
+          if (element == userInput) {
+            myBooks.remove(element);
+          }
+        });
+      }
+      break;
+    case 6:
+      {
+        myBooks.forEach((element) {
+          if (element == userInput) {
+            print(element);
+          }
+        });
+      }
+  }
+}
